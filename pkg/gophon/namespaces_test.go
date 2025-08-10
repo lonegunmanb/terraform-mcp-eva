f7bc45a6870757cc021d2bc3afd4b3f3066e11f8
zjhe <lonegunmanb@hotmail.com>
2025-08-10 15:22:53 +0800

    add azuread source code index, use github go sdk instead of hand craft http get to read source code so we can use github token as authentication token

--- pkg/gophon/namespaces_test.go
+++ pkg/gophon/namespaces_test.go
@@ -29,39 +29,3 @@ func TestListSupportedNamespaces(t *testing.T) {
 		})
 	}
 }
-
-func TestGetNamespaceBaseURL(t *testing.T) {
-	tests := []struct {
-		name        string
-		namespace   string
-		expectedURL string
-		expectError bool
-	}{
-		{
-			name:        "should return base URL for azurerm namespace",
-			namespace:   "github.com/hashicorp/terraform-provider-azurerm/internal",
-			expectedURL: "https://raw.githubusercontent.com/lonegunmanb/terraform-provider-azurerm-index/refs/{version}/index",
-			expectError: false,
-		},
-		{
-			name:        "should return error for unsupported namespace",
-			namespace:   "unsupported.namespace",
-			expectedURL: "",
-			expectError: true,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			url, err := GetNamespaceBaseURL(tt.namespace)
-
-			if tt.expectError {
-				assert.Error(t, err)
-				assert.Empty(t, url)
-			} else {
-				assert.NoError(t, err)
-				assert.Equal(t, tt.expectedURL, url)
-			}
-		})
-	}
-}
